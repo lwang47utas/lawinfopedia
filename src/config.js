0fd6e041b876644992dca93271ffaf2d01706dc1
lwang47utas <126302290+lwang47utas@users.noreply.github.com>
2024-10-08 17:25:19 +0800

    测试一下

--- src/config.js
+++ src/config.js
@@ -7,6 +7,11 @@ export const APPURL =
 // 图片转发的url
 export const PNGURL = process.env.NODE_ENV === 'development' ? '/glimg' : 'http://10.85.222.17:3002/'
 // 地图url
+// 地图url
 export const GEOURL = process.env.NODE_ENV === 'development' ? 'http://221.122.67.145:8323/' : 'http://10.85.222.16:8323/'
 export const GEOURL2 = process.env.NODE_ENV === 'development' ? 'http://221.122.67.145:8323/' : 'http://10.85.222.16:8323/'
 export const GEOURL3 = process.env.NODE_ENV === 'development' ? 'http://221.122.67.145:8889/' : 'http://10.85.222.16:8889/'
+
+
+
+
